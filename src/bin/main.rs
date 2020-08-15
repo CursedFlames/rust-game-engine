ff33bfd6310669c7a72f68a1cc01933a7a468b25
CursedFlames <18627001+CursedFlames@users.noreply.github.com>
2020-08-15 14:54:39 +1200

    minor refactoring into functions

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,10 +2,22 @@ use std::sync::Arc;
 
 use vulkano::device::{Device, DeviceExtensions, Features, QueuesIter};
 use vulkano::instance::{Instance, InstanceExtensions, PhysicalDevice};
+use vulkano::swapchain::Surface;
 use vulkano_win::VkSurfaceBuild;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
-use winit::window::WindowBuilder;
+use winit::window::{Window, WindowBuilder};
+
+use vulkan_test::vulkutil;
+
+fn create_window(instance: &Arc<Instance>) -> (EventLoop<()>, Arc<Surface<Window>>) {
+	let events_loop = EventLoop::new();
+	let surface = WindowBuilder::new()
+		.with_title("Vulkan")
+		.build_vk_surface(&events_loop, instance.clone())
+		.unwrap();
+	(events_loop, surface)
+}
 
 fn main() {
 	let instance = {
@@ -14,16 +26,7 @@ fn main() {
 			.expect("Failed to create Vulkan instance.")
 	};
 
-	let devices: Vec<PhysicalDevice> = PhysicalDevice::enumerate(&instance).collect();
-
-	println!("{} devices found:", devices.len());
-	for dev in &devices {
-		println!("{:?} ({}, type: {:?})", dev, dev.name(), dev.ty());
-	}
-
-	let physical = *devices.get(0).expect("Failed to find any available devices");
-
-	println!("Selected device: {}", physical.name());
+	let physical = vulkutil::select_physical_device(&instance);
 
 	let queue_family = physical.queue_families()
 		.find(|&q| q.supports_graphics())
@@ -39,15 +42,21 @@ fn main() {
 	// We only have one queue
 	let queue = queues.next().unwrap();
 
-
-	let events_loop = EventLoop::new();
-	let surface = WindowBuilder::new().build_vk_surface(&events_loop, instance.clone()).unwrap();
+	let (events_loop, surface) = create_window(&instance);
 
 	events_loop.run(|event, _, control_flow| {
+		// TODO this might break things once we're actually rendering to the surface
+		//      for now it just serves to prevent max CPU usage on one thread
+		*control_flow = ControlFlow::Wait;
+		// Commented out to prevent console spam
+		// println!("event {:?}", event);
 		match event {
 			Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
 				*control_flow = ControlFlow::Exit;
 			},
+			Event::WindowEvent { event: WindowEvent::Resized(size), ..} => {
+				println!("resized {:?}", size);
+			}
 			_ => ()
 		}
 	});
---
+++ src/lib.rs
@@ -0,0 +1,2 @@
+// This could really do with a better name, but hey, doesn't matter for now I guess
+pub mod vulkutil;
