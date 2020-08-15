1e25b391440748cab1f04e496a31c0d4464c7810
CursedFlames <18627001+CursedFlames@users.noreply.github.com>
2020-08-15 18:14:49 +1200

    shaders, render pass, and pipeline - just more or less copied from the vulkano example really

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,8 +2,10 @@ use std::sync::Arc;
 
 use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
 use vulkano::device::{Device, DeviceExtensions, Features, Queue, QueuesIter};
+use vulkano::framebuffer::{RenderPass, Subpass};
 use vulkano::image::{ImageUsage, SwapchainImage};
-use vulkano::instance::{Instance, InstanceExtensions, PhysicalDevice};
+use vulkano::instance::{Instance, PhysicalDevice};
+use vulkano::pipeline::GraphicsPipeline;
 use vulkano::swapchain::{ColorSpace, FullscreenExclusive, PresentMode, Surface, SurfaceTransform, Swapchain};
 use vulkano_win::VkSurfaceBuild;
 use winit::event::{Event, WindowEvent};
@@ -96,13 +98,14 @@ fn main() {
 	let (mut swapchain, images) =
 		create_swapchain(physical, &device, &surface, &queue);
 
+	// TODO move this Vertex struct to somewhere more sensible
+	//      ideally move all this buffer stuff somewhere more sensible, really.
+	#[derive(Default, Debug, Clone)]
+	struct Vertex {
+		position: [f32; 2],
+	}
+
 	let vertex_buffer = {
-		// TODO move this Vertex struct to somewhere more sensible
-		//      ideally move all this buffer stuff somewhere more sensible, really.
-		#[derive(Default, Debug, Clone)]
-		struct Vertex {
-			position: [f32; 2],
-		}
 		vulkano::impl_vertex!(Vertex, position);
 
 		CpuAccessibleBuffer::from_iter(
@@ -117,6 +120,68 @@ fn main() {
 			].iter().cloned()
 		).unwrap()
 	};
+
+	// TODO figure out where to put shaders
+	//      apparently they don't rebuild when changed unless other things in the file are also changed?
+	//      irritating if true.
+	mod vs {
+		vulkano_shaders::shader! {
+			ty: "vertex",
+			src: "\
+#version 450
+layout(location = 0) in vec2 position;
+void main() {
+	gl_Position = vec4(position, 0.0, 1.0);
+}"
+		}
+	}
+
+	mod fs {
+		vulkano_shaders::shader! {
+			ty: "fragment",
+			src: "\
+#version 450
+layout(location = 0) out vec4 f_color;
+void main() {
+	f_color = vec4(0.1, 0.25, 1.0, 1.0);
+}"
+		}
+	}
+
+	let vs = vs::Shader::load(device.clone()).unwrap();
+	let fs = fs::Shader::load(device.clone()).unwrap();
+
+	let render_pass: Arc<RenderPass<_>> = Arc::new(
+		vulkano::single_pass_renderpass!(
+			device.clone(),
+			attachments: {
+				color: {
+					// attachment is cleared upon draw
+					load: Clear,
+					store: Store,
+					format: swapchain.format(),
+					// no idea what this does, but the vulkano example uses it
+					samples: 1,
+				}
+			},
+			pass: {
+				color: [color],
+				depth_stencil: {}
+			}
+		).unwrap()
+	);
+
+	let pipeline = Arc::new(
+		GraphicsPipeline::start()
+			.vertex_input_single_buffer::<Vertex>()
+			.vertex_shader(vs.main_entry_point(), ())
+			.triangle_list()
+			.viewports_dynamic_scissors_irrelevant(1)
+			.fragment_shader(fs.main_entry_point(), ())
+			.render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
+			.build(device.clone())
+			.unwrap()
+	);
 
 	events_loop.run(|event, _, control_flow| {
 		// TODO this might break things once we're actually rendering to the surface
