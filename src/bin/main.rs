ec00c59017cf3fe37b0a51b2f0a6dbef04b3473b
CursedFlames <18627001+CursedFlames@users.noreply.github.com>
2020-08-15 17:11:30 +1200

    add a swapchain. not used yet

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,8 +1,9 @@
 use std::sync::Arc;
 
-use vulkano::device::{Device, DeviceExtensions, Features, QueuesIter};
+use vulkano::device::{Device, DeviceExtensions, Features, Queue, QueuesIter};
+use vulkano::image::{ImageUsage, SwapchainImage};
 use vulkano::instance::{Instance, InstanceExtensions, PhysicalDevice};
-use vulkano::swapchain::Surface;
+use vulkano::swapchain::{ColorSpace, FullscreenExclusive, PresentMode, Surface, SurfaceTransform, Swapchain};
 use vulkano_win::VkSurfaceBuild;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
@@ -19,6 +20,37 @@ fn create_window(instance: &Arc<Instance>) -> (EventLoop<()>, Arc<Surface<Window
 	(events_loop, surface)
 }
 
+fn create_swapchain(physical: PhysicalDevice, device: &Arc<Device>, surface: &Arc<Surface<Window>>, queue: &Arc<Queue>)
+		-> (Arc<Swapchain<Window>>, Vec<Arc<SwapchainImage<Window>>>) {
+	let caps = surface.capabilities(physical).unwrap();
+	// TODO we probably want to actually pick this properly?
+	//      Seems to normally be opaque, but shouldn't rely on that.
+	let alpha = caps.supported_composite_alpha.iter().next().unwrap();
+	println!("Using alpha mode {:?}", alpha);
+	// TODO formats?
+	let format = caps.supported_formats[0].0;
+	println!("Using format {:?}", format);
+
+	let dimensions: [u32; 2] = surface.window().inner_size().into();
+
+	Swapchain::new(
+		device.clone(),
+		surface.clone(),
+		caps.min_image_count,
+		format,
+		dimensions,
+		1,
+		ImageUsage::color_attachment(),
+		queue,
+		SurfaceTransform::Identity,
+		alpha,
+		PresentMode::Fifo,
+		FullscreenExclusive::Default,
+		true,
+		ColorSpace::SrgbNonLinear
+	).unwrap()
+}
+
 fn main() {
 	let instance = {
 		let extensions = vulkano_win::required_extensions();
@@ -28,21 +60,40 @@ fn main() {
 
 	let physical = vulkutil::select_physical_device(&instance);
 
+	let (events_loop, surface) = create_window(&instance);
+
+	// println!("Available queue families:");
+	// let queue_families = physical.queue_families();
+	// for family in queue_families {
+	// 	println!("{} {} {} {} {}",
+	// 		family.queues_count(),
+	// 		family.supports_graphics(),
+	// 		family.supports_compute(),
+	// 		family.explicitly_supports_transfers(),
+	// 		family.supports_sparse_binding());
+	// }
+
 	let queue_family = physical.queue_families()
 		.find(|&q| q.supports_graphics())
 		.expect("Failed to find a graphical queue family");
 
 	println!("Selected queue family: {:?}", queue_family);
 
+	let device_ext = DeviceExtensions {
+		khr_swapchain: true,
+		..DeviceExtensions::none()
+	};
 	let (device, mut queues) = {
-		Device::new(physical, &Features ::none(), &DeviceExtensions::none(),
+		Device::new(physical, &Features ::none(), &device_ext,
 					[(queue_family, 0.5)].iter().cloned()).expect("Failed to create device")
 	};
 
 	// We only have one queue
+	// TODO use multiple queues?
 	let queue = queues.next().unwrap();
 
-	let (events_loop, surface) = create_window(&instance);
+	let (mut swapchain, images) =
+		create_swapchain(physical, &device, &surface, &queue);
 
 	events_loop.run(|event, _, control_flow| {
 		// TODO this might break things once we're actually rendering to the surface
