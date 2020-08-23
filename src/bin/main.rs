0ebf5b558fdafba64e415e3467fec34baa15b589
CursedFlames <18627001+CursedFlames@users.noreply.github.com>
2020-08-23 21:44:26 +1200

    switch to spin_sleep for frame timing
    it behaves very well; just need to add a wrapper or separate struct for handling tick scheduling.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,7 @@
 use std::thread;
 use std::time::{Duration, Instant};
 
+use spin_sleep::LoopHelper;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 
@@ -10,9 +11,14 @@ use vulkan_test::util::timing::Timing;
 fn main() {
 	let events_loop = EventLoop::new();
 	let mut renderer = Renderer::init(&events_loop);
-	let mut timing = Timing::new();
+	// let mut timing = Timing::new();
+	let mut timer = LoopHelper::builder()
+		.report_interval_s(0.5)
+		.build_with_target_rate(60.0);
 
 	// let mut tick_count = 0;
+
+	let mut time = 0.0;
 
 	events_loop.run(move |event, _, control_flow| {
 		// Not used since it means framerate is kept low unless events are occurring
@@ -31,15 +37,26 @@ fn main() {
 			},
 			Event::RedrawEventsCleared => {},
 			Event::MainEventsCleared => {
-				timing.wait_for_next_frame();
-				/*let delta = */timing.on_update();
-				let time = timing.get_total_time();
+				// timing.wait_for_next_frame();
+				// /*let delta = */timing.on_update();
+				// let time = timing.get_total_time();
 
-				// TODO actually do tick stuff
-				while timing.try_consume_tick() {
-					// println!("tick {}", tick_count);
-					// tick_count += 1;
+				timer.loop_sleep();
+
+				let delta = timer.loop_start();
+				time += delta.as_secs_f64();
+
+				if let Some(fps) = timer.report_rate() {
+					println!("FPS: {}", fps);
 				}
+
+				// // TODO actually do tick stuff
+				// while timing.try_consume_tick() {
+				// 	// println!("tick {}", tick_count);
+				// 	// tick_count += 1;
+				// }
+
+				// let time = timer.
 
 				renderer.draw_frame(time as f32);
 			},
